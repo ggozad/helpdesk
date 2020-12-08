f2e277109710b44c77611fdae650b318e79f6cd2
Yiorgis Gozadinos <ggozad@crypho.com>
2020-12-08 13:41:34 +0100

    JWT tokens, crypto

---
+++ backend/backend/crypto.py
@@ -0,0 +1,21 @@
+from datetime import datetime, timedelta
+import os
+from typing import Mapping
+from jose import JWTError, jwt
+
+
+SECRET = os.environ.get("AUTH_SECRET")
+
+
+def tokenize(data: dict, expires: timedelta = None) -> str:
+    if expires is not None and data.get("exp") is None:
+        data["exp"] = datetime.utcnow() + expires
+    return jwt.encode(
+        data,
+        SECRET,
+        algorithm="HS256",
+    )
+
+
+def detokenize(token: str) -> Mapping:
+    return jwt.decode(token, SECRET)
