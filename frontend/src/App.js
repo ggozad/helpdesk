2b03c22408123efc2f32d2576f94ba02d1b27b00
Yiorgis Gozadinos <ggozad@crypho.com>
2020-12-10 12:28:14 +0100

    React router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,22 @@
 import React from 'react'
-import Login from './views/login'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import Login from './views/agentlogin'
+import Register from './views/register'
+import Home from './views/home'
 
 export default function App() {
-  return <Login />
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact={true} component={Home} />
+        <Route
+          path="/agent/register/:token"
+          exact={true}
+          component={Register}
+        />
+        <Route path="/agent/" exact={true} component={Login} />
+      </Switch>
+    </Router>
+  )
 }
